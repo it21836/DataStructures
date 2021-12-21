9445b28786acfbfc2838fab26dae00ee05b1e370
Kalliopi Despoina Manouseli <77158945+it21860@users.noreply.github.com>
2021-12-21 18:28:30 +0200

    First Deliverable
    First Deliverable

--- assignementOne/src/test/java/org/hua/assignementOne/CircularTest.java
+++ assignementOne/src/test/java/org/hua/assignementOne/CircularTest.java
@@ -1,19 +1,15 @@
-package org.hua.assignementOne;
+package org.hua.circularqueue;
 
-import static org.junit.Assert.assertTrue;
-
-import org.hua.assignementOne.CircularQueue;
-import org.hua.assignementOne.Queue;
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 /**
- * Unit test for simple App.
+ * Unit tests.
  */
-public class AppTest 
+public class CircularTest 
 {
 	@Test
 	public void testCircularQueue() {
-		Queue<Integer> q = new CircularQueue<>();
+		CircularQueue<Integer> q = new CircularQueue<>();
 		
 		assertTrue(q.isEmpty());
 
@@ -41,23 +37,23 @@ public class AppTest
 		//check if is empty
 		assertTrue(q.isEmpty());
 		
-		for (int i = 0; i < q.getArrayLenght(); i++) {
+		for (int i = 0; i < q.getArrayLength(); i++) {
 			q.enqueue(i);
 		}
 		
-		int firstlenght=q.getArrayLenght();
-		int firstsize=q.size();
+		int firstLength=q.getArrayLength();
+		int firstSize=q.size();
 		//ckeck if is full
-		assertTrue(q.size()==q.getArrayLenght());
+		assertTrue(q.size()==q.getArrayLength());
 		
 		//push another element
 		q.enqueue(10);
 		
 		//check if the array has doubled
-		assertTrue(q.getArrayLenght()== 2*firstlenght);
+		assertTrue(q.getArrayLength()== 2*firstLength);
 		
 		//check if the size of elements has augmented 
-		assertTrue(q.size()==firstsize+1);
+		assertTrue(q.size()==firstSize+1);
 		
 		
 	}
@@ -70,25 +66,24 @@ public class AppTest
 		assertTrue(q.isEmpty());
 		
 		//fill the queue
-		for (int i = 0; i < q.getArrayLenght(); i++) {
+		for (int i = 0; i < q.getArrayLength(); i++) {
 			q.enqueue(i);
 		}
 		
-		int firstsize=q.size();
+		int firstSize=q.size();
 		
-		//ckeck if is full
-		assertTrue(q.size()==q.getArrayLenght());
+		//check if is full
+		assertTrue(q.size()==q.getArrayLength());
 		
 		//leave less that 1/4 of elements inside queue
-		int s= (3*q.getArrayLenght())/4;
+		int s= (3*q.getArrayLength())/4;
 		for(int i=0;i<=s;i++) {
 			q.dequeue();
 		}
 		
 		//check if the capacity has become half of what it was
-		assertTrue(q.getArrayLenght()==(firstsize/2));
-		
-		
+		assertTrue(q.getArrayLength()==(firstSize/2));
+			
 	}
 	
 }
