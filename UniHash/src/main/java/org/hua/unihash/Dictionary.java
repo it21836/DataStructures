7b225bf4d5f8892b95607e83015ea0e13f184092
Kalliopi Despoina Manouseli <77158945+it21860@users.noreply.github.com>
2022-01-17 18:49:13 +0200

    Second Deliverable
    Second Deliverable

--- UniHash/src/main/java/org/hua/unihash/Dictionary.java
+++ UniHash/src/main/java/org/hua/unihash/Dictionary.java
@@ -2,7 +2,7 @@
  * This code is part of the exercises for the Data Structures course at Harokopio
  * University of Athens, Dept. of Informatics and Telematics.
  *
- * @author Manouseli Kalliopi Despoina - Karakasidou Aikaterini
+ * @author 21860 | 21836
  */
 package org.hua.unihash;
 
@@ -16,93 +16,26 @@ import java.util.Iterator;
  */
 public interface Dictionary<K, V> extends Iterable<Dictionary.Entry<K, V>> {
 
-    /**
-     * Put a key value pair
-     *
-     * @param key a key
-     * @param value a value
-     */
     void put(K key, V value);
 
-    /**
-     * Remove a mapping
-     *
-     * @param key the key
-     * @return the value
-     */
     V remove(K key);
 
-    /**
-     * Get the value associated with a key
-     *
-     * @param key the key
-     * @return the value or null
-     */
     V get(K key);
-
-    /**
-     * Check if a key exists
-     *
-     * @param key the key
-     * @return true if it exists, false otherwise
-     */
+    
     boolean contains(K key);
-
-    /**
-     * Check if empty
-     *
-     * @return true if empty, false otherwise
-     */
     
     boolean isEmpty();
-    /* 
-    or
-    
-    default boolean isEmpty() {
-        return size() == 0;
-    }
-    */
-    
 
-    /**
-     * Get the number of elements Πόσα ζευγάρια key-value έχω μέσα
-     *
-     * @return the number of elements
-     */
     int size();
 
-    /**
-     * Clear
-     */
     void clear();
 
-    /**
-     * Get an iterator.
-     *
-     * @return an iterator
-     */
     Iterator<Entry<K, V>> iterator();
 
-    /**
-     * A dictionary entry.
-     *
-     * @param <K> the key type
-     * @param <V> the value type
-     */
     interface Entry<K, V> {
 
-        /**
-         * Get the key
-         *
-         * @return the key
-         */
         K getKey();
 
-        /**
-         * Get the value
-         *
-         * @return the value
-         */
         V getValue();
     }
 }
